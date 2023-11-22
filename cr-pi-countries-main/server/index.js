fc0bce1adae066848781de93a87b68c3986b0733
PHRCOOK <pabloelleproso@gmail.com>
2023-11-22 13:19:46 -0300

    BASE DE DATOS Y SERVER

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -1,10 +1,16 @@
 const axios = require("axios");
 const server = require("./src/server");
-const { conn } = require('./src/db.js');
+const { conn } = require("./src/db.js");
 const PORT = 3001;
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+const populateDb = require("./src/controllers/populateDb.js");
+
+// Syncing all the models at once.
+conn.sync({ alter: true, force: false }).then(() => {
+  //con {force: true} se dropean todas las tablas al levantar el server
+  server.listen(PORT, async () => {
+    //Cuando se levanta el servidor se ejecuta populateDB para poblar la base de datos
+    await populateDb();
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
+});
