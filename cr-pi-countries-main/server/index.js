0258baa79f1074d745cfe48b4dcafdb8f9c69b0c
PHRCOOK <pabloelleproso@gmail.com>
2023-11-22 14:44:56 -0300

    MODIF

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
 const PORT = 3001;
@@ -10,6 +9,6 @@ conn.sync({ alter: true, force: false }).then(() => {
   server.listen(PORT, async () => {
     //Cuando se levanta el servidor se ejecuta populateDB para poblar la base de datos
     await populateDb();
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log("Servidor levantado en el puerto: " + PORT); // eslint-disable-line no-console
   });
 });
