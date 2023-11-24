9328021234ace906a13dde3f077a593451e49092
PHRCOOK <pabloelleproso@gmail.com>
2023-11-24 02:59:39 -0300

    modif

--- cr-pi-countries-main/client/vite.config.js
+++ cr-pi-countries-main/client/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
-export default {
-  build: {
-    outDir: "build",
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+});
