4adc7d666dcb6e114b42faf33c08d226a4b07bf5
PHRCOOK <pabloelleproso@gmail.com>
2023-11-30 12:52:35 -0300

    modif

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -1,15 +1,16 @@
+require("dotenv").config({ path: "./api/.env" });
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
+// const PORT = 3001;
 
 const populateDb = require("./src/controllers/populateDb.js");
 
 conn.sync({ alter: true, force: false }).then(() => {
   //con {force: true} se dropean todas las tablas al levantar el server
-  server.listen(PORT, async () => {
+  server.listen(process.env.PORT, async () => {
     //Cuando se levanta el servidor se ejecuta populateDB para poblar la base de datos
     await populateDb();
-    console.log("Servidor levantado en el puerto: " + PORT); // eslint-disable-line no-console
+    console.log("Servidor levantado en el puerto: 3001"); // eslint-disable-line no-console
   });
 });
