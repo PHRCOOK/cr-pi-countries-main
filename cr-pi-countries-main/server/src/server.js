e5b77951c7665ba29ad5098c34554f44d8af5c39
PHRCOOK <pabloelleproso@gmail.com>
2023-11-29 22:22:36 -0300

    deploy

--- cr-pi-countries-main/server/src/server.js
+++ cr-pi-countries-main/server/src/server.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://cr-pi-countries-main-alpha.vercel.app/"
+    "https://cr-pi-countries-main-alpha.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
