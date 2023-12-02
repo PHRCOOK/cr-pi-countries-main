598616505156dcbbbd84bf69422aaf2905ac7384
PHRCOOK <pabloelleproso@gmail.com>
2023-12-02 00:18:41 -0300

    deploy

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -8,7 +8,7 @@ const populateDb = require("./src/controllers/populateDb.js");
 
 conn.sync({ alter: true, force: false }).then(() => {
   //con {force: true} se dropean todas las tablas al levantar el server
-  server.listen(process.env.PORT, async () => {
+  server.listen(`${BASE_API_URL}`, async () => {
     //Cuando se levanta el servidor se ejecuta populateDB para poblar la base de datos
     await populateDb();
     console.log("Servidor levantado en el puerto:", process.env.PORT); // eslint-disable-line no-console
