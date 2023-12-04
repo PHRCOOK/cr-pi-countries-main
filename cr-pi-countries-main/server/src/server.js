1099e2d140c7622f54e1cd65e76b788deafb7eaa
PHRCOOK <pabloelleproso@gmail.com>
2023-12-04 10:45:46 -0300

    m

--- cr-pi-countries-main/server/src/server.js
+++ cr-pi-countries-main/server/src/server.js
@@ -15,11 +15,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://cr-pi-countries-main-xi.vercel.app"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
