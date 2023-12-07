170b12273a4c1595743667f5f57358024b237b12
PHRCOOK <pabloelleproso@gmail.com>
2023-12-07 20:51:10 -0300

    M

--- cr-pi-countries-main/client/src/main.jsx
+++ cr-pi-countries-main/client/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./components/css-modules/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
