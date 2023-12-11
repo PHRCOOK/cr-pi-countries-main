656eb8f9f78d39bcc7a48f5c13644f984d145ba5
PHRCOOK <pabloelleproso@gmail.com>
2023-12-10 23:38:48 -0300

    M

--- cr-pi-countries-main/client/src/redux/thunks.js
+++ cr-pi-countries-main/client/src/redux/thunks.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { deleteActivity } from "./actions";
 
+//manejo de eliminacion de las actividades
+
 export const deleteActivityThunk = (id) => async (dispatch) => {
   await axios.delete(`/activities/${id}`);
   dispatch(deleteActivity(id));
