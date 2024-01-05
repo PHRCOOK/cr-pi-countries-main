c003ea57874002e4817d6509904e379e16699bfe
PHRCOOK <pabloelleproso@gmail.com>
2024-01-04 22:38:58 -0300

    CSS

--- cr-pi-countries-main/server/src/controllers/populateDb.js
+++ cr-pi-countries-main/server/src/controllers/populateDb.js
@@ -9,10 +9,8 @@ const populateDb = async () => {
   try {
     console.log("Reading API...");
     //realiza la peticion a la API
-    // const { data } = await axios(`http://localhost:5000/countries`);
-
-    // BASE DE DATOS REMOTA
-    const { data } = await axios(`${BASE_API_URL}/countries`);
+    const { data } = await axios(`http://localhost:5000/countries`);
+    // const { data } = await axios(`${BASE_API_URL}/countries`); BASE DE DATOS REMOTA
     //prepara un arreglo con todos los datos obtenidos de la api
     console.log("Preparing data package...");
     const bulckData = data?.map((country) => {
