e103a4162c7ecabfda350a5b38274c4ebcdad454
PHRCOOK <pabloelleproso@gmail.com>
2024-01-16 14:09:04 -0300

    M

--- cr-pi-countries-main/server/index.js
+++ cr-pi-countries-main/server/index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 
 const populateDb = require("./src/controllers/populateDb.js");
 
-conn.sync({ alter: true, force: true }).then(() => {
+conn.sync({ alter: true, force: false }).then(() => {
   //con {force: true} se dropean todas las tablas al levantar el server
   server.listen(process.env.PORT, async () => {
     //Cuando se levanta el servidor se ejecuta populateDB para poblar la base de datos
